43f6b2f5176aee8f22072520349e72241de4897b
Filip Mares <fmares@sessionm.com>
2022-03-04 15:57:26 +0100

    normalization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,36 @@
+const pkg = require('./package.json');
+const DESC = 'KSČM je strana minulosti. Její představitelé mají na svědomí mnoho hříchů z předlistopadové i polistopadové éry.';
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.volby-kscm.cz",
-    title: "Komunisti z kola ven",
+    title: pkg.description,
+    description: DESC,
+    author: pkg.author,
+    siteUrl: pkg.homepage,
   },
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
+        name: pkg.description,
+        short_name: pkg.name,
+        description: DESC,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
         icon: "src/images/icon.png",
       },
     },
+    'gatsby-plugin-sass',
     {
-      resolve: "gatsby-plugin-mdx",
+      resolve: 'gatsby-plugin-svgr',
       options: {
-        defaultLayouts: {
-          default: require.resolve("./src/components/layout.jsx")
-        }
-      }
+        svgo: false,
+        ref: true,
+      },
     },
+    `gatsby-plugin-react-helmet`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -39,6 +46,52 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    `gatsby-plugin-image`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: require.resolve(`@nrwl/gatsby/plugins/nx-gatsby-ext-plugin`),
+      options: {
+        path: __dirname,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    "gatsby-plugin-sitemap",
+    `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.tsx")
+        },
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 550,
+            }
+          },
+        ],
+        rehypePlugins: [
+          {
+            resolve: 'rehype-slug'
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          'G-2DBHBVRPLQ'
+        ],
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        pluginConfig: {
+          head: false,
+        },
+      },
     },
   ],
 };
---
+++ src/components/layout.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql, Link } from "gatsby";
+import clasNames from "classnames";
+
+import * as styles from "./layout.module.scss";
+
+const Layout = ({ pageContext, children }) => {
+  const { site: { siteMetadata }} = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
+  const TITLE = `${pageContext.frontmatter.title} | ${siteMetadata.title}`;
+  const DESC = pageContext.frontmatter.description || siteMetadata.description;  
+
+  return (
+    <div className={styles.page}>
+      <Helmet
+        htmlAttributes={{ lang: 'cs' }}
+        title={TITLE}
+        meta={[
+          {
+            name: `description`,
+            content: DESC,
+          },
+          {
+            property: `og:title`,
+            content: TITLE,
+          },
+          {
+            property: `og:description`,
+            content: DESC,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:title`,
+            content: TITLE,
+          },
+          {
+            name: `twitter:description`,
+            content: DESC,
+          },
+        ]}
+      />
+      <main className={styles.main} data-testid={pageContext.frontmatter.theme}>
+        <div className={styles.menu}>
+          <input type="checkbox" id="swith" />
+          <label htmlFor="swith" />
+          <ul>
+            <li><Link to="/">Úvod</Link></li>
+            <li><Link to="/ideologie">Ideologie</Link></li>
+            <li><Link to="/komunismus-v-cesku">Komunismus v česku</Link></li>
+            <li><Link to="/porevolucni-kscm">Porevoluční KSČM</Link></li>
+            <li><Link to="/predstavitele-kscm">Představitelé KSČM</Link></li>
+          </ul>
+        </div>
+        <section className={clasNames(styles.section, pageContext.frontmatter.theme)}>
+          {children}
+        </section>
+      </main>
+      <footer className={styles.footer}>
+        <small>Podporujeme: <a href="https://www.nasdilejneztozakazou.cz/">Sdílejte, než to zakážou! pravá tvář Andreje Babiše</a> a <a href="https://www.petletzpet.cz/">Největší přešlapy současného prezidenta</a></small>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
